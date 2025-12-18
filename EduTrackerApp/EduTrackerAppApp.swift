5a72c319485d5b6e068c759f04f2f01ce1a04a96
Adilet <adiletbeishekeyev@gmail.com>
2025-12-18 10:59:55 +0500

    Initial Commit

---
+++ EduTrackerApp/EduTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduTrackerAppApp.swift
+//  EduTrackerApp
+//
+//  Created by Adilet Beishekeyev on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct EduTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
